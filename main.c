d9fa2b106bec7c968eff25f4a156875c7dd618f9
Amit Barzilai <amit.barzilai22@gmail.com>
2025-11-19 08:33:09 +0200

    Started hooking tcp_v4_rcv

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/fs.h>
+#include <linux/tcp.h>
 #include "trampoline/trampoline.h"
 
 
@@ -9,24 +9,24 @@ MODULE_AUTHOR("Amit Barzilai");
 MODULE_DESCRIPTION("Trampoline hook");
 
 
-static int (*orig_iterate_dir)(struct file *, struct dir_context *);
+static int (*orig_tcp_v4_rcv)(struct sk_buff *);
 
-static int hooked_iterate_dir(struct file *f, struct dir_context *dc)
+static int hooked_tcp_v4_rcv(struct sk_buff *skb)
 {
     printk(KERN_ALERT "Get hooked douchebag!\n");
-    return orig_iterate_dir(f, dc);
+    return orig_tcp_v4_rcv(skb);
 }
 
-static const hook_t iterate_dir_hook = {
-    .target_name = "iterate_dir",
-    .original_function = (void *)&orig_iterate_dir,
-    .function = hooked_iterate_dir
+static const hook_t tcp_v4_rcv_hook = {
+    .target_name = "tcp_v4_rcv",
+    .original_function = (void *)&orig_tcp_v4_rcv,
+    .function = hooked_tcp_v4_rcv
 };
 
-static int hook_ls_init(void)
+static int captain_init(void)
 {
     int err;
-    err = tramp_hook_install(&iterate_dir_hook);
+    err = tramp_hook_install(&tcp_v4_rcv_hook);
     if (err) {
         printk(KERN_ALERT "Failed installing hook\n");
         return err;
@@ -35,11 +35,11 @@ static int hook_ls_init(void)
 	return 0;
 }
 
-static void hook_ls_exit(void)
+static void captain_exit(void)
 {
-    tramp_hook_uninstall(&iterate_dir_hook);
+    tramp_hook_uninstall(&tcp_v4_rcv_hook);
 	printk(KERN_INFO "Removed iterate_dir hook\n");
 }
 
-module_init(hook_ls_init);
-module_exit(hook_ls_exit);
+module_init(captain_init);
+module_exit(captain_exit);
